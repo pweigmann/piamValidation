90d00c445d8a4acb9f6d1b4536b0307b4ffd8516
Pascal Weigmann <pascal.weigmann@pik-potsdam.de>
2024-04-18 14:39:42 +0200

    deal with reference values being zero

--- R/evaluateThresholds.R
+++ R/evaluateThresholds.R
@@ -15,6 +15,11 @@ evaluateThresholds <- function(df, cleanInf = TRUE) {
       # relative deviation above/below reference
       (value - ref_value) / ref_value
       )
+    ) %>%
+    # special case: ref_value and value are both zero should show as 0 deviation
+    mutate(check_value = ifelse(value == 0 & ref_value == 0,
+                                0,
+                                check_value)
     )
 
   # difference ####
@@ -55,25 +60,38 @@ evaluateThresholds <- function(df, cleanInf = TRUE) {
   # perform comparison to thresholds for whole data.frame at once
   # TODO: not as robust as previously thought. Partially fails if only max_red is given
   df <- df %>%
-    mutate(check = ifelse(is.na(check_value),
-                        "grey",
-                        ifelse(
-                          # first check whether red threshold is violated...
-                          check_value > max_red | check_value < min_red,
-                          "red",
-                          # otherwise check if yellow threshold is violated...
+    mutate(check = ifelse(is.na(check_value) | is.infinite(check_value),
+                          "grey",
                           ifelse(
-                            check_value > max_yel | check_value < min_yel,
-                            "yellow",
-                            # ... else green
-                            "green"
+                            # first check whether red threshold is violated...
+                            check_value > max_red | check_value < min_red,
+                            "red",
+                            # otherwise check if yellow threshold is violated...
+                            ifelse(
+                              check_value > max_yel | check_value < min_yel,
+                              "yellow",
+                              # ... else green
+                              "green"
                             )
                           )
                         )
            )
 
+  if (any(is.infinite(df$check_value))) {
+    cat(
+    "A relative check to a reference value of zero was performed. Make sure you
+    use the right reference data or try checking for a difference instead. \n")
+  }
+
   # after evaluation, "Inf" can be removed
-  if (cleanInf) df[df == "Inf" | df == "-Inf"] <- NA
+  if (cleanInf) {
+    df <- df %>%
+      mutate(min_red = ifelse(is.infinite(min_red), NA, min_red),
+             min_yel = ifelse(is.infinite(min_yel), NA, min_yel),
+             max_yel = ifelse(is.infinite(max_yel), NA, max_yel),
+             max_red = ifelse(is.infinite(max_red), NA, max_red)
+             )
+  }
 
   return(df)
 }
